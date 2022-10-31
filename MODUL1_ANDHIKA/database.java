9a54c227fbedecec6ab242c2142e902cce7bc002
andhikavanes <116347178+andhikavanes@users.noreply.github.com>
2022-10-31 08:43:19 +0700

    Add files via upload

---
+++ MODUL1_ANDHIKA/database.java
@@ -0,0 +1,11 @@
+package OOP-OLLA-ANDHIKA-1202213203.MODUL1_ANDHIKA;
+
+import java.util.Scanner;
+
+public class database {
+    Scanner nm new Scanner(System.in);
+    System.out.println("Nama :");
+    str nama=nm.next();
+
+
+}
